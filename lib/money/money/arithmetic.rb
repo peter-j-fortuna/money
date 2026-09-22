604c67e96509d79a526a6a815fb83479dd9e8c2b
Peter Fortuna <peter.j.fortuna@gmail.com>
2026-09-22 13:10:13 -0400

    Correct eql? method warning
    This warning assumes the currencies are not equal. It would be best to make that explicit.

--- lib/money/money/arithmetic.rb
+++ lib/money/money/arithmetic.rb
@@ -39,7 +39,7 @@ class Money
     # @see Money.strict_eql_compare
     def eql?(other)
       if other.is_a?(Money)
-        if !Money.strict_eql_compare && fractional == 0 && other.fractional == 0
+        if !Money.strict_eql_compare && fractional == 0 && other.fractional == 0 && currency != other.currecy
           warn "[DEPRECATION] Comparing 0 #{currency} with 0 " \
                "#{other.currency} using `#eql?` will return false in " \
                "future versions of Money. Opt-in to the new behavior by " \
